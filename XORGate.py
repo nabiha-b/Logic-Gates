4f7343dabff3b200b8846557c092d024051630f6
Nabiha B <47676025+nabiha-b@users.noreply.github.com>
2020-06-01 23:50:32 -0700

    Create XORGate.py

---
+++ XORGate.py
@@ -0,0 +1,14 @@
+# Nabiha Bashir
+# This program stimulates a 2 input XOR Gate
+
+#-------XOR Gate-------
+print("XOR Gate")
+# Get A & B values
+xorA = int(input("Enter a value for A (1 or 0): "))
+xorB = int(input("Enter a value for B (1 or 0): "))
+# Display A & B
+print("Your inputs A =", xorA, "and B =", xorB)
+if (xorA != xorB):
+    print("Make your statement TRUE \n")
+else:
+    print("Make your statement FALSE \n")
