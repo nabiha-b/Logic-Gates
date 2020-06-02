65cd3b639d0d9e8d84dc9f3173d8a7f48f63434c
Nabiha B <47676025+nabiha-b@users.noreply.github.com>
2020-06-01 23:49:47 -0700

    Create NOTGate.py

---
+++ NOTGate.py
@@ -0,0 +1,18 @@
+# Nabiha Bashir
+# This program stimulates a 2 input NOT Gate
+
+#-------NOT Gate-------
+print("NOT Gate")
+# Get A & B values
+notA = int(input("Enter a value for A (1 or 0): "))
+notB = int(input("Enter a value for B (1 or 0): "))
+# Display A & B
+print("Your inputs A =", notA, "and B =", notB)
+if (notA == 0):
+    print("Make your statement for A TRUE ")
+elif (notA == 1):
+    print("Make your statement for A FALSE ")
+if (notB == 0):
+    print("Make your statement for B TRUE \n")
+elif (notB == 1):
+    print("Make your statement for B FALSE \n")
