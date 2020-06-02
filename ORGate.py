9427832caf3be3d5c4d7246750236f93376f82c1
Nabiha B <47676025+nabiha-b@users.noreply.github.com>
2020-06-01 23:45:35 -0700

    Create ORGate.py

---
+++ ORGate.py
@@ -0,0 +1,16 @@
+# Nabiha Bashir
+# This program stimulates a 2 input OR Gate
+
+#-------OR Gate-------
+print("OR Gate")
+# Get A & B values
+orA = int(input("Enter a value for A (1 or 0): "))
+orB = int(input("Enter a value for B (1 or 0): "))
+# Display A & B
+print("Your inputs A =", orA, "and B =", orB)
+if (orA == 1):
+    print("Make your statement TRUE \n")
+elif (orB == 1):
+    print("Make your statement TRUE \n")
+else:
+    print("Make your statement FALSE \n")
